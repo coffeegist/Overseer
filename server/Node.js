064077f9b2d02129608b11ef8f1a446bc1e3e0e0
Adam Brown <audrummer15@gmail.com>
2016-01-25 11:09:07 -0600

    * Added ClassC function to backend Node objects.

--- server/Node.js
+++ server/Node.js
@@ -15,5 +15,18 @@ Node.prototype.toJSON = function() {
   };
 
   return result;
-}
+};
+
+Node.prototype.isAddressClassC = function() {
+  var self = this;
+  var result = false;
+  var firstOctet = Number(self._ip.substring(0,3));
+
+  if (firstOctet >= 192 && firstOctet <= 223) {
+    result = true;
+  }
+
+  return result;
+};
+
 module.exports = Node;
